76cad85555eeeded3789e2a88a0e02028000cf29
MohamedSafan2004 <123774344+MohamedSafan2004@users.noreply.github.com>
2026-05-25 04:42:58 +0300

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,14 @@ import Providers from "./providers"
 export const metadata: Metadata = {
   title: "2Z Store",
   description: "Minimal Streetwear",
-  icons: {
-    icon: "/logo.jpeg",
-    apple: "/logo.jpeg",
-  },
+icons: {
+  icon: [
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon.ico" },
+  ],
+  apple: "/logo.jpeg",
+},
 }
 
 export default function RootLayout({
