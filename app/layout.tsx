04a80343d734265d9cf790b9bd83c5af94904f7c
MohamedSafan2004 <123774344+MohamedSafan2004@users.noreply.github.com>
2026-05-25 04:51:53 +0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,7 @@ export const metadata: Metadata = {
   title: "2Z Store",
   description: "Minimal Streetwear",
 icons: {
-  icon: [
-    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    { url: "/favicon.ico" },
-  ],
-  apple: "/logo.jpeg",
+  icon: "/favicon.ico",
 },
 }
 
